401ba5ba7e88340a36f6a5a2b51086ac0952b194
Eugenio Pandu Faulin <pandufaulin@gmail.com>
2024-05-22 01:06:46 -0300

    Correccion a la estructura de las carpetas

--- src/Data/usuarioData.ts
+++
@@ -1,1 +0,0 @@
-//Objeto UsuarioData que contendra metodos para conectar, subir y consultar datos de la base de datos.
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { Usuario } from "./Models/usuarioModel.js";
+import { Usuario } from "./Classes/usuarioClass.js";
 
 const port = 3000;
 const app = express();
