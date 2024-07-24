bfcf17b87e2e76a5f79187e52b7fa4a9135faf04
Eugenio Pandu Faulin <pandufaulin@gmail.com>
2024-07-23 21:49:16 -0300

    Manejo de errores para CRUD Usuarios en implementados en sus Routes

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,13 @@
 import express from "express";
 import { usuarioRouter } from "./routes/usuarioRoute.js";
+import { logger } from "./logger.js";
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
 
-//        get -> obtener recurso
-//       post -> crear nuevo recurso
-//     delete -> borrar recurso
-//put & patch -> modificar recurso
-
-/* Pequeño logger para depuerar y comprobar peticiones a recursos
-0 -> Sin log
-1 -> Muestra peticion
-2 -> Muestra peticion + protocolo + direccion de acceso
-*/
-const logger:number = 1;
-if (logger > 0) {
-    app.use(function(req, res, next) {
-        if (logger == 1) {
-            console.log("Acceso -> " + req.path);
-        } else {
-            console.log(`Acceso -> ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-        }
-        next();
-    });
-}
+logger(1, app);
 
 app.use("/api/usuario/", usuarioRouter);
 
--- src/routes/usuarioRoute.ts
+++ src/routes/usuarioRoute.ts
@@ -1,94 +1,112 @@
 import { Router, Request, Response } from "express";
-import { UsuarioRepository } from "../repository/usuarioRepository.js";
-import { Usuario } from "../entities/usuarioEntity.js";
 import { UsuarioController } from "../controllers/usuarioController.js";
 
 export const usuarioRouter = Router();
 
+class Return {
+    static Update(res:Response) {
+        return res.status(200).send("Usuario Actualizado");
+    };
+    static Delete(res:Response) {
+        return res.status(202).send("Usuario Borrado")
+    };
+    static BadInput(res:Response) {
+        return res.status(400).send("Que pones?????");
+    };
+    static NotFound(res:Response) {
+        return res.status(404).send("No se encontro ID");
+    };
+    static Generic(res:Response) {
+        return res.status(500).send("(。_。)");
+    };
+}
+
 usuarioRouter.route("/")
     //GetAll
     .get(async function(req, res) {
-        const usuarios = await UsuarioController.GetAll();
-        //Falta validar error
-        return res.json(usuarios);
+        try {
+            const usuarios = await UsuarioController.GetAll();
+            return res.json(usuarios);
+        } catch {
+            return Return.Generic(res);
+        }
     })
     //Create
     .post(async function(req, res) {
-        const {usuario, contra, email, tipo} = req.body;
-
         //Verifico inputs
-        if (!usuario || !contra || !email || !tipo) {
-            return res.status(400).send("Que pones?????");
+        if (!UsuarioController.Inputs(req)) {
+            return Return.BadInput(res);
         }
 
-        const newUsr = UsuarioController.New(usuario, contra, email, tipo);
-        const result = await UsuarioController.Create(newUsr);
+        const {usuario, contra, email, tipo} = req.body;
 
-        //Falta validar error
-        return res.status(201).send(result);
+        try {
+            const newUsr = UsuarioController.New(usuario, contra, email, tipo);
+            const result = await UsuarioController.Create(newUsr);
+
+            return res.status(201).send(result);
+        } catch {
+            return Return.Generic(res);
+        }
     })
 ;//END usuario
 
 usuarioRouter.route("/:id")
     //GetOne
     .get(async function(req, res) {
-        //const id = Number.parseInt(req.params.id);
         const id = req.params.id;
 
-        //Verifico inputs
-        /*if (isNaN(id)) {
-            return res.status(400).send("Que pones?????");
-        }*/
-
-        //¿Existe ID?
-        const tmpUsr = await UsuarioController.GetOne(id);
-        if (tmpUsr) {
-            return res.status(201).json(tmpUsr);
+        try {
+            //¿Existe ID?
+            const tmpUsr = await UsuarioController.GetOne(id);
+            if (tmpUsr) {
+                return res.status(201).json(tmpUsr);
+            }
+            return Return.NotFound(res);
+        } catch {
+            return Return.Generic(res);
         }
-        return res.status(404).send("No se encontro ID");
     })
     //Update
     .put(async function(req, res) {
-        //const id = Number.parseInt(req.params.id);
+        //Verifico inputs
+        if (!UsuarioController.Inputs(req)) {
+            return Return.BadInput(res);
+        }
+
         const id = req.params.id;
         const {usuario, contra, email, tipo} = req.body;
 
-        //Verifico inputs
-        if (!usuario || !contra || !email || !tipo) {
-            return res.status(400).send("Que pones?????");
-        }
-
-        //¿Existe ID?
-        const tmpUsr = await UsuarioController.GetOne(id);
-        if (tmpUsr) {
-            const updUsr = UsuarioController.New(usuario, contra, email, tipo, id);
-            if (await UsuarioController.Update(updUsr)) {
-                return res.status(200).send("Usuario Actualizado");
+        try {
+            //¿Existe ID?
+            const tmpUsr = await UsuarioController.GetOne(id);
+            if (tmpUsr) {
+                const updUsr = UsuarioController.New(usuario, contra, email, tipo, id);
+                if (await UsuarioController.Update(updUsr)) {
+                    return Return.Update(res);
+                }
+                return Return.Generic(res);
             }
-            return res.status(500).send("(。_。)");
+            return Return.NotFound(res);
+        } catch {
+            return Return.Generic(res);
         }
-        return res.status(404).send("No se encontro ID");
     })
     //Delete
     .delete(async function(req, res) {
-        //const id = Number.parseInt(req.params.id);
         const id = req.params.id;
 
-        //Verifico inputs
-        /*if (isNaN(id)) {
-            return res.status(400).send("Que pones?????");
-        }*/
-
-        //¿Existe ID?
-        const tmpUsr = await UsuarioController.GetOne(id);
-        if (tmpUsr) {
-            if (await UsuarioController.Delete(id)) {
-                return res.status(202).send("Usuario Borrado");
+        try {
+            //¿Existe ID?
+            const tmpUsr = await UsuarioController.GetOne(id);
+            if (tmpUsr) {
+                if (await UsuarioController.Delete(id)) {
+                    return Return.Delete(res);
+                }
             }
+            return Return.NotFound(res);
+        } catch {
+            return Return.Generic(res);
         }
-        return res.status(404).send("No se encontro ID");
     })
 ;//END usuario/:id
-
-//Nos estamos adelantando...
-//Se deja de base para su uso con bases de datos
