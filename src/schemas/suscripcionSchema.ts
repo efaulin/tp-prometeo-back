d39069127962832ead4b53447b9fd0d26a2c90ba
Eugenio Pandú Faulin <87616215+efaulin@users.noreply.github.com>
2024-09-11 22:41:43 +0000

    WIP CRUD Suscripcion

--- src/schemas/suscripcionSchema.ts
+++ src/schemas/suscripcionSchema.ts
@@ -1,14 +1,19 @@
-import { getModelForClass, getName, prop, Ref } from '@typegoose/typegoose';
+import { getModelForClass, getName, prop, Ref, index } from '@typegoose/typegoose';
 import { time } from 'console';
 import mongoose from 'mongoose';
 
 //Para las relaciones debiles se utiliza un enfoque de one-to-many como referencia, para evitar mal rendimiento en caso de un gran historico de precios
+@index({ startDate: 1, suscripcionId: 1 }, { unique: true })
 class SuscripcionPrecio {
     @prop({ required: true, default: new Date() })
     public startDate!: Date;
 
     @prop({ required: true, default: 0 })
     public amount!: Number;
+
+    //Se agrega id de la suscripcion para poder crear el indice, y no poder agregar dos precios con la misma fecha
+    @prop({ required: true })
+    public suscripcionId!: string;
 }
 
 class Suscripcion {
